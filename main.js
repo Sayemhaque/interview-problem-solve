57fbb6e9e071d054c80d9d23ae010714539bc7bd
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-02-25 01:12:19 +0600

    meal deatails modal & api calling added

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-;
 const inputField = document.getElementById("input-field");
 
-
-function loadData (name) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
-  .then((response) => response.json())
-  .then((json) => {
-    const stringData = JSON.stringify(json);
-    const data = JSON.parse(stringData);
-    displayData(data.meals);
-  });
+function loadData(name) {
+  try{
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+      displayData(data.meals);
+    });
+  }
+  catch(error) {
+    console.log(error)
+  }
 }
 
 function displayData(json) {
   const container = document.querySelector(".container");
-   container.innerHTML = ''
-    
-   json.forEach(data => {
+  container.innerHTML = "";
+
+  json.forEach((data) => {
     const div = document.createElement("div");
     div.innerHTML = `
       <div class="card card-compact w-full bg-base-100 shadow-xl">
@@ -24,37 +25,41 @@ function displayData(json) {
       <div class="card-body">
         <h2 class="card-title">${data.strCategory}</h2>
         <p>${data.strMeal}</p>
-        <div class="card-actions justify-end">
-        <label for="my-modal-6" onclick="mealModalDetails(${data.idMeal})" class="btn">Details</label>
+        <label for="my-modal-6"
+         onclick="loadMealModalDetails(${data.idMeal})" class="btn">Details</label>
+        </div>
+        </div>
+  
 
-        <!-- Put this part before </body> tag -->
-        <input type="checkbox" id="my-modal-6" class="modal-toggle" />
-        <div class="modal modal-bottom sm:modal-middle">
-          <div class="modal-box">
-          <figure><img src=${data.strMealThumb} alt="Shoes" /></figure>
-            <h3 class="font-bold text-3xl">${data.strMeal}</h3>
-            <p class="py-4">${data.strInstructions}</p>
-            <div class="modal-action">
-              <label for="my-modal-6" class="btn">Close</label>
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-      </div
       `;
     container.appendChild(div);
-  })
+  });
 }
 
+function searchMeal() {
+  loadData(inputField.value);
+}
+const img = document.getElementById("meal-img");
+const title = document.getElementById("meal-title");
+const description = document.getElementById("meal-description");
 
-
-
-function searchMeal () {
-  loadData(inputField.value)
+function loadMealModalDetails(idMeal) {
+  img.src = "";
+  title.innerText = "";
+  description.innerText = "";
+  try {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    .then((response) => response.json())
+    .then((data) => displayMealModalDetails(data.meals[0]));
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-function mealModalDetails (idMeal) {
-  console.log(idMeal)
+function displayMealModalDetails(meal) {
+  img.src = meal.strMealThumb;
+  title.innerText = meal.strMeal;
+  description.innerText= meal.strInstructions;
 }
-loadData("")
+
+loadData("rice");
