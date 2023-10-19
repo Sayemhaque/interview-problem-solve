9430d45f38f69608f8f624d3b34721784591090a
Mohammad sayem Haque <85791327+Sayemhaque@users.noreply.github.com>
2023-10-19 11:11:28 +0600

    solved the first problem which i can not solved

--- index.js
+++ index.js
@@ -3,30 +3,33 @@ const app = express()
 const employee_data = require("./data")
 
 
-app.get("/" , async(req,res) => {
+app.get("/", async (req, res) => {
     res.send("working")
 })
 
 
-app.get("/employeeData",async(req,res) => {
+app.get("/employeeData", async (req, res) => {
     res.send(employee_data)
 })
 
-app.get("/employee/:id", async(req,res) => {
-    const id = req.params.id;
-    const employee = employee_data.employees.find(id => id === id)
+app.get("/employee/:id", async (req, res) => {
+    const Id = req.params.id;
+    const employee = employee_data.employees.find(employee => employee.id == Id)
     res.send(employee)
 })
-app.get("/employee", async(req,res) => {
-    const employeeId = req.query.employeeId
-    const employeeObj = req.params.employeeObj
-    console.log(employeeId,employeeObj)
-    const employeeSingle = employee_data.employees.find(id => id === employeeId)
-    const result  = employeeSingle?.employee?.find(id => id == 4)
-    console.log(result)
-    res.send(result)
+app.get("/employee", async (req, res) => {
+    try {
+        const employeeId = req.query.employeeId
+        const employeeObjId = req.query.employeeObjId
+        console.log(employeeId)
+        const employeeSingle = employee_data.employees.find(employee => employee.id === employeeId)
+        const result = employeeSingle.employee.find(employee => employee.id === employeeObjId)
+        res.send(result)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("server is running")
 })
